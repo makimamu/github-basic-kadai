8a3ea5a3767b7f2169711dbac3ef2ae5e073e6c7
makimamu <156180587+makimamu@users.noreply.github.com>
2024-04-25 00:30:46 +0900

    kadai₋０07のアップ

--- kadai₋007/calc.js
+++ kadai₋007/calc.js
@@ -1,13 +1,13 @@
 let num =15
-if (num % 3 ===0 && num % 5=== 0) {
+if (num % 3 === 0 && num % 5=== 0) {
   console.log("3と5の倍数です")
 }
 else if (num % 3 === 0) {
-  console.log("3の倍数である")
+  console.log("3の倍数です")
 }
-else  if  (num % 5 === 0) {
-  console.log("5の倍数である")
+else if (num % 5 === 0) {
+  console.log("5の倍数です")
 }
 else {
-  console.log("num")
+  console.log("15")
 }
