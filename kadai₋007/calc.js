2dac423078f4125b5c1b8a0ef113bc43cb2a21db
makimamu <156180587+makimamu@users.noreply.github.com>
2024-04-26 01:35:52 +0900

    kadai₋０07のアップ

--- kadai₋007/calc.js
+++ kadai₋007/calc.js
@@ -1,6 +1,8 @@
-let num =15
-if (num % 3 === 0 && num % 5=== 0) {
-  console.log("3と5の倍数です")
+//変数numにランダムな整数を代入
+let num = Math.floor(Math.random() * 15);
+
+if (num % 3 === 0 && num % 5 ===0 ){
+  console.log("3と5の倍数です" )
 }
 else if (num % 3 === 0) {
   console.log("3の倍数です")
@@ -9,5 +11,5 @@ else if (num % 5 === 0) {
   console.log("5の倍数です")
 }
 else {
-  console.log("15")
+  console.log("num")
 }
